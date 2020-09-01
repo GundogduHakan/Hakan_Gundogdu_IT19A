65bd28dd754a3b9350d099e34120bbf8f4599c51
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-09-01 09:40:01 +0200

    error med DOM

--- Programmering 1/Introkodning/index.js
+++ Programmering 1/Introkodning/index.js
@@ -2,5 +2,16 @@
 
 // skapar en variabel som heter name och tilldelar det värdet "Hakan"
 let name = "Hakan"
+let age = 18 //heltatl,int
 
-console.log(`Hej ${name}`)
+console.log(`Hej ${name} du är ${age} år gammal`)
+// if.sats - att göra val
+if (age >= 18){
+    console.log("Du är vuxen")
+}
+ else {
+    console.log("Du är barn")
+}
+ // DOM - Documennt Object Model
+ let text = document.querySelector ("text")
+ text.innerHTML = ${name} "är" ${age} "år gammal"
