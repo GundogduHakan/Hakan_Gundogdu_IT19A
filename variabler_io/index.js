a501565dc879f8862279e1c01dcbd946e1f48dbf
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-09-08 09:39:33 +0200

    räknat medelvärde

---
+++ variabler_io/index.js
@@ -0,0 +1,21 @@
+// skapa en variabel name och tilldela ditt namn
+
+//skriv ut "Hej name"
+let name = "Hakan"
+console.log (`hej ${name} `)
+
+// skapa variablerna tal1, tal2, tal3
+// tilldela dessa variabler med värderna 3,4,5
+// räkna ut medelvärdet av dem
+
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+// räkna ut medelvärdet av dem
+medelvärde = (tal1+tal2+tal3)/3
+
+// gör följande utskrift:
+// "Medelvärdet av talen 3,4,5 är "
+console.log (`${tal1}+${tal2}+${tal3}/3) = ${medelvärde}`)
+
+let langd = prompt("ange för rektangelns längd")
