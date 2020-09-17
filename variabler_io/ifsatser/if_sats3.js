70cc98d972fb56f650767e558e20dc0f6e676125
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-09-17 09:56:40 +0200

    klar med lektionen

--- variabler_io/ifsatser/if_sats3.js
+++ variabler_io/ifsatser/if_sats3.js
@@ -1,10 +1,12 @@
 let antal = prompt ("Hur många gånger åker du spår varje månad?")
 
-if (antal < 26){
+if (antal*30<775){
     console.log("Du bör inte köpa en månadskort")
+    console.log(`Det kostar ${antal*30} kr med engångsbiljett`)
 }
 else {
     console.log("Du bör köpa en månadskort, du sparar pengar på det.")
+    console.log(`Det kostar ${antal*30} kr med engångsbiljett`)
 }
 
 
