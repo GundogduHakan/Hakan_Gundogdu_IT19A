23388b8b5d48fcf2718074a034f92ba5c8aefd08
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-09-17 09:53:09 +0200

    IF_SATSER

---
+++ variabler_io/ifsatser/if_sats2.js
@@ -0,0 +1,27 @@
+console.log("hej detta är if_sats lektion 2")
+
+
+let age = prompt ("Hur gammal är du?")
+
+if (age >= 15){
+    console.log("du får se på filmer med 15-årsgräns")
+}
+else{
+    console.log("du får inte kolla på filmer med 15-årsgräns")
+}
+
+if (age >= 18){
+    console.log("du får se filmer med 18-årsgrän")
+}
+else{
+    console.log("du får inte se filmer med 18års-gräns")
+}
+
+if (age >= 7){
+    console.log("du får se filmer med 7-årsgräns")
+}
+else[
+    console.log("du får inte se filmer med 7-årsgräns")
+]
+
+
---
+++ variabler_io/ifsatser/if_sats3.js
@@ -0,0 +1,10 @@
+let antal = prompt ("Hur många gånger åker du spår varje månad?")
+
+if (antal < 26){
+    console.log("Du bör inte köpa en månadskort")
+}
+else {
+    console.log("Du bör köpa en månadskort, du sparar pengar på det.")
+}
+
+
