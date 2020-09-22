3e903a9547ae4f7ac9d6745fa918834c802acd5d
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-09-22 09:12:57 +0200

    Jag har gjort SAS uppgiften (If_Sats4

---
+++ variabler_io/ifsatser/if_sats4.js
@@ -0,0 +1,38 @@
+let vikt = prompt ("Hur mycket väger ditt bagage?")
+
+if (vikt>8){
+    console.log("Du får inte passera, max vikt är 8kg.")
+}
+else {
+    console.log("Du får passera.")
+}
+
+
+let Längden = prompt ("Längden på ditt bagage?")
+
+
+
+if (Längden >55){
+    console.log("Du får inte passera, max längd är 55cm")
+}
+else {
+    console.log("Du får passera")
+}
+
+let bredden = prompt ("Bredden på ditt bagage")
+
+if (bredden >40){
+    console.log("Du får inte passera, max bredd är 40cm")
+}
+else{
+    console.log("Du får passera")
+}
+
+let höjden = prompt ("Höjden på din bagage=")
+
+if (höjden >23){
+    console.log("Du får inte passera, max höjd är 23cm")
+}
+else(
+    console.log("Du får passera")
+)
