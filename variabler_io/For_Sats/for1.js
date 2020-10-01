589d5960ddcc139939fe36f2224b7d44613c9646
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2020-10-01 09:49:57 +0200

    Läxa For-Sats1

--- variabler_io/For_Sats/for1.js
+++ variabler_io/For_Sats/for1.js
@@ -2,33 +2,33 @@ let p_talserie1 = document.querySelector("#talserie1")
 let p_talserie2 = document.querySelector("#talserie2")
 let p_summa = document.querySelector("#summa")
 let p_klass = document.querySelector("#klasslista")
+let p_Udda = document.querySelector("#Udda")
+let p_Jämna = document.querySelector("#Jämna")
 let summa = 0
-for (let i=1; i<=10; i++){
-    p_talserie1.innerHTML += `${i} `
+
+Jämna.innerHTML = "Jämna tal från 0 till 100: ";
+for(let i=0; i<=100; i++){
+    if( i % 2==0){
+    Jämna.innerHTML += `${i},`;
+    }
+}
+Udda.innerHTML = "Udda tal från 0 till 100: "
+for(let j=1; j<=100; j++){
+    if(j % 2==1){
+        Udda.innerHTML += `${j},`;
+    }
 }
 
 
-for (let j=10; j>=0; j--){
-    p_talserie2.innerHTML += `${j} `
-}
-
-for (let i = 0; i<10; i++){
-    console.log(`${i}. I will not write all over the walls`)
-}
-
-// vill räkna ut summan 1+2+3+...+8+9+10
-for (let i=0; i<=10; i++){
-    summa += i
-}
-
-p_summa.innerHTML += `1 + 2 +3 +...+9+10= ${summa}`
-let elever = ["Kokchun ", "Henrik" , "Thomas"]
-
-// skapa en lista med elever
 
 
-p_klass.innerHTML = "Klass IT19A  innehåller följande elever <br/>"
+let ämnen = ["Svenska", "Engelska" , "Programmering" , "Matematik" , "Idrott"]
 
-for (let k =0; k < elever.length; k++){
-    p_klass.innerHTML += `${k+1} ${elever[k]} <br/>`
+// skapa en lista med lektioner
+
+
+p_klass.innerHTML = "Mina lektioner är <br/>"
+
+for (let k =0; k < ämnen.length; k++){
+    p_klass.innerHTML += `${k+1} ${ämnen[k]} <br/>`
 }
