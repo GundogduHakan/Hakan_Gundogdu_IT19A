bd6bd7a3afd49975a28814634525ad5680db0030
GundogduHakan <70194279+GundogduHakan@users.noreply.github.com>
2021-03-09 09:07:01 +0100

    repetition

---
+++ Programmering 1/repetition/repetition.js
@@ -0,0 +1,27 @@
+console.log("Repetition v10")
+
+
+// skapa variabler
+let namn = "kokchun"
+let ålder = "18"
+console.log(`hej ${namn} du är ${ålder} år gammal`)
+
+// if - sats
+
+if (ålder => 18  ){
+    console.log("Du är vuxen")
+} else{
+    console.log("du är barn")
+}
+
+// for -sats skriva ut jämna tal 0-100
+
+for (let i = 0; i <20; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
+    
+
+    
+    
